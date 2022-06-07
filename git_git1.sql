82d6bb638719f62b5d36deb8202e16985edca0b0
Rodrigo Regadío Núñez <83121658+regadior@users.noreply.github.com>
2022-06-07 15:38:30 +0200

    Update git_git1.sql
    Prueba 2

--- git_git1.sql
+++ git_git1.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `git1`
 --
-
+#prueba
 DROP TABLE IF EXISTS `git1`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
