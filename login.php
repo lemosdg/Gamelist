a973b5b6782688d1ea1581c2b0b7def36df32b50
Diego Lemos <diegolemosgarcia@gmail.com>
2022-06-20 12:44:48 +0200

    Login created

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta cherset="utf-8">
+        <title>Login</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <h1>Login</h1>
+
+        <form action="login.php" method="post">
+            <input type="text" name="email" placeholder="Enter your mail">
+            <input type="text" name="password" placeholder="Enter your password">
+            <input type="submit" value="send">
+        </form>
+    </body>
+</html>
