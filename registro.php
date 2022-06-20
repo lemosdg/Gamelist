6a6d71add3a66994a075f760fa35592e5b072ac2
Diego Lemos <diegolemosgarcia@gmail.com>
2022-06-20 12:32:19 +0200

    index and css created

--- registro.php
+++ registro.php
@@ -1,3 +1,17 @@
-<?php
-    echo "works";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta cherset="utf-8">
+        <title>Welcome to your App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <h1>Please Login or sing up</h1>
+
+        <a href="login.php">Login</a> or
+        <a href="signup.php">Signup</a>
+    </body>
+</html>
