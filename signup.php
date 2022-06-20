d3c028956666944162e033778d26fd01888f014d
Diego Lemos <diegolemosgarcia@gmail.com>
2022-06-20 13:06:33 +0200

    signup header

---
+++ signup.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta cherset="utf-8">
+        <title>Signup</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <?php require "partials/header.php" ?>
+    
+    </body>
+</html>
